d02e1534d228b1a5ba16e66ff0d66abad8213383
Sokofreta <Seitanidissokratis@gmail.com>
2026-06-28 15:43:21 +0300

    Creation of a custom page not found

---
+++ Client/src/components/Pages/Pagenotfound/PageNotFound.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../Navbar/Navbar"
+import "./PageNotFound.css"
+
+const PageNotFound = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <div className="PageNotFound">
+                <h1>
+                    Page not found
+                </h1>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default PageNotFound
