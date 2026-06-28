7baaa10734be9842f1bd25b9a3f7aee7bd823943
Sokofreta <Seitanidissokratis@gmail.com>
2026-06-28 15:51:07 +0300

    Updating navbar component
    Move Navbar from left to the top of the screen

--- Client/src/components/Pages/Navbar/Navbar.jsx
+++ Client/src/components/Pages/Navbar/Navbar.jsx
@@ -1,62 +1,55 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse,faBriefcase,faTrowel, faUserGear } from '@fortawesome/free-solid-svg-icons';
+import { faUserGear } from '@fortawesome/free-solid-svg-icons';
 import "./Navbar.css"
 
 const Navbar = () => {
   const NAVIGATE = useNavigate();
 
-  const [isHover, SetisHover] = useState(false)
-
 
   return (
-    <div className='Navbar' onMouseOver={() => { SetisHover(true) }} onMouseOut={() => { SetisHover(false) }} >
-      <div className='Logo'>
-        <p>LOGO</p>
-      </div>
-      
-      <div className='NavLinkCon'>
-        <button className="Navlink" onClick={() => { NAVIGATE("/") }}>
-          <FontAwesomeIcon icon={faHouse} size='2xl' />
-        </button>
-        {isHover ? <p>Αρχικη</p> : ""}
-      </div>
+    <div className='Navbar'>
+
 
       <div className='NavLinkCon'>
-        <button className="Navlink" onClick={() => { NAVIGATE("/Scrapper") }}>
-          <FontAwesomeIcon icon={faTrowel} size='2xl' />
+        <button className="Navlink" onClick={() => { NAVIGATE("/") }}>
+          {/* <FontAwesomeIcon icon={faHouse} size='2xl' /> */}
+          Αρχική σελίδα
         </button>
-        {isHover ? <p>Scrapper</p> : ""}
+
+      </div>
+
+      {/* <div className='NavbarSep'>
+        
+      </div> */}
+      <div className='NavLinkCon'>
+        <button className="Navlink" onClick={() => { NAVIGATE("/Scrapper") }}>
+          {/* <FontAwesomeIcon icon={faTrowel} size='2xl' /> */}
+          Scrapper
+        </button>
+
       </div>
 
 
       <div className='NavLinkCon'>
         <button className="Navlink" onClick={() => { NAVIGATE("/Bussineses") }}>
-          <FontAwesomeIcon icon={faBriefcase} size='2xl' />
+          {/* <FontAwesomeIcon icon={faBriefcase} size='2xl' /> */}
+          Επιχειρήσεις
         </button>
-        {isHover ? <p>Επιχειρήσεις</p> : ""}
+
       </div>
 
 
-      <div className='NavLinkCon'>
-        <button className="Navlink" onClick={() => { NAVIGATE("/Profile") }}>
+      <div className="NavLinkCon">
+        <button className="UserProfile " onClick={() => { NAVIGATE("/Profile") }}>
           <FontAwesomeIcon icon={faUserGear} size='2xl' />
         </button>
-        {isHover ? <p>Προφιλ</p> : ""}
       </div>
+
 
 
 
     </div>
-    // <div className='Navbar'>
-    //   <Button variant='outlined' id="Navlink" onClick={() => {NAVIGATE("/")}}> ΑΡΧΙΚΗ ΣΕΛΙΔΑ</Button>
-    //   <Button variant='outlined' id="Navlink" onClick={() => {NAVIGATE("/Scrapper")}}>Scrapper</Button>
-    //   <Button variant='outlined' id="Navlink" onClick={() => {NAVIGATE("/Bussineses")}}>ΕΠΙΧΕΙΡΗΣΕΙΣ</Button>
-    //   <Button variant='outlined' id="Navlink" onClick={() => {NAVIGATE("/Register")}}>Συνδεση/Εγγραφη</Button>
-    //   <Button variant='outlined' id="Navlink" onClick={() => {NAVIGATE("/Profile")}}>Προφιλ</Button>
-
-    // </div>
   )
 }
 
